8f60094a36d1b4b784ebe6bd6b3015811a5f3a85
Dugina80 <alenka251280d@gmail.com>
2023-05-01 15:16:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/*const one = 0.1;
+const two = 0.2;
+const sum = (one+two).toFixed(1);
+console.log (sum)
+
+const one = "1";
+const two = 2;
+const sum = +(one) + two;
+console.log (sum);
+
+const flashDriveVolume = prompt ('enter the size of the flash drive in GB');
+const calculator = 1000;
+const fileSize = 820;
+const resalt = ((flashDriveVolume * calculator) / fileSize).toFixed(0);
+alert (`fit ${resalt}`);
+
+const amountMoney = prompt ('The amount of money in the wallet');
+const chocolatePrice = prompt ('chocolate price');
+const quantity = Math.floor (amountMoney / chocolatePrice);
+const resalt= (amountMoney - (quantity * chocolatePrice)) .toFixed(2);
+alert (`The rest of money ${resalt}`);
+
+const depositAmount = +prompt ('enter amount');
+const annualPercentage = 0.05;
+const percentageMonth = (annualPercentage / 12) .toFixed (4);
+const amountFirstMonth = depositAmount + ( depositAmount * percentageMonth);
+const amountSecondMonth = amountFirstMonth + (amountFirstMonth* percentageMonth);
+const amountAccruedInterest = (amountSecondMonth - depositAmount) .toFixed (2);
+alert (`amount of accrued interest ${amountAccruedInterest}`);
+
+/*2 && 0 && 3 поверне 0
+2 || 0 || 3  поверне 2
+2 && 0 || 3  поверне 3*/
+
+
+
+
